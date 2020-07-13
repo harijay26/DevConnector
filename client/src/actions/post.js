1362567433bf5f95e7972d4d9056ee94169f0842
Promise Harry <chinecherempharry@gmail.com>
2020-07-13 18:01:47 +0200

    working on Post actions

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -2,7 +2,10 @@ import axios from 'axios';
 import { setAlert } from './alert';
 import { 
     GET_POSTS,
-    POST_ERROR
+    POST_ERROR,
+    UPDATE_LIKES,
+    DELETE_POST,
+    ADD_POST
 } from './types';
 
 
@@ -19,6 +22,97 @@ export const getPosts = () => async (dispatch) => {
         dispatch({
             type: POST_ERROR,
             payload: { 
+                msg: err.response.statusText, 
+                status: err.response.status 
+            }
+        });
+    }
+};
+
+// Add like
+export const addLike = postId => async (dispatch) => {
+    try {
+        const res = await axios.put(`/api/posts/like/${postId}`);
+
+        dispatch({
+            type: UPDATE_LIKES,
+            payload: { postId, likes: res.data }
+        });
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: {  
+                msg: err.response.statusText, 
+                status: err.response.status 
+            }
+        });
+    }
+};
+
+// Remove like
+export const removeLike = postId => async (dispatch) => {
+    try {
+        const res = await axios.put(`/api/posts/unlike/${postId}`);
+
+        dispatch({
+            type: UPDATE_LIKES,
+            payload: { postId, likes: res.data }
+        });
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: {  
+                msg: err.response.statusText, 
+                status: err.response.status 
+            }
+        });
+    }
+};
+
+// Delete post   
+export const deletePost = id => async (dispatch) => {
+    try {
+        await axios.delete(`/api/posts/${id}`);
+
+        dispatch({
+            type: DELETE_POST,
+            payload: id
+        });
+
+        dispatch(setAlert('Post Removed', 'success'));
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: {  
+                msg: err.response.statusText, 
+                status: err.response.status 
+            }
+        });
+    }
+};
+
+// Add post   
+export const addPost = formData => async (dispatch) => {
+
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+
+    try {
+        const res = await axios.post(`/api/posts`, formData, config);
+
+        dispatch({
+            type: ADD_POST,
+            payload: res.data
+        });
+
+        dispatch(setAlert('Post Created', 'success'));
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: {  
                 msg: err.response.statusText, 
                 status: err.response.status 
             }
