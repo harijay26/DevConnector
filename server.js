b81147e204c43f84237465e18aee8b08bf302be6
Promise Harry <chinecherempharry@gmail.com>
2020-07-14 09:10:17 +0200

    Prepared for deployment v2

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json({
     extended: false
 }))
 
-//app.get('/', (req, res) => res.send('API Running'));
+
 
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
